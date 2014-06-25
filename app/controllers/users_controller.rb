1def83a0a0fbe7816d1c46bb7309d86527720262
WarInUkraine <warinukraine.info@gmail.com>
2014-06-25 17:09:43 +0300

    profile editing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,34 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_user
 
   def profile
-    @user = current_user
   end
+
+  def edit
+  end
+
+  def update
+    user_update_params = user_params
+
+    if user_update_params[:password].empty?
+      user_update_params.delete(:password)
+      user_update_params.delete(:password_confirmation)
+    end
+
+    if @user.update(user_update_params)
+      redirect_to profile_path, notice: 'Профиль успешно отредактироан!'
+    else
+      render :edit
+    end
+  end
+
+  private
+    def set_user
+      @user = current_user
+    end
+
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :about, :password, :password_confirmation)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -32,6 +32,8 @@ Rails.application.routes.draw do
   end
 
   get 'profile', to: 'users#profile'
+  get 'profile/edit', to: 'users#edit'
+  patch 'profile/update', to: 'users#update'
 
   match '/:url', url: /.*/, constraints: { url: /((?!assets)\w.+)+/ }, to: 'statics#display', via: :get
 end
